c63601fbb07ab9df1bae8af411b1e1b29e71935f
Angel Barajas <Barajasb15@gmail.com>
2022-06-01 04:10:41 -0500

    add: Vista Login

--- SistemaDeportivo/Startup.cs
+++ SistemaDeportivo/Startup.cs
@@ -50,7 +50,7 @@ namespace SistemaDeportivo
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
